2efeec3750e1a6dd281f643e496afe1140bb5869
Wellington Maciel <welgt.maciel@gmail.com>
2020-05-03 20:38:20 -0300

    melhoria na implementacao da rotacao e posicao da nave

--- LunarLander.py
+++ LunarLander.py
@@ -1,4 +1,39 @@
 import pygame
-from NewObjeto import *
+from NovoObjeto import *
+from NovaTela import *
 
-nave = New_objeto('arquivos/nave.png')
+tela = Nova_tela('teste', ((600,600)))
+nave = Novo_objeto('arquivos/bola.jpg', (550,550))
+nave.set_tamanho((50,50))
+
+print("nave.get_tamanho() :", nave.get_tamanho())
+print("nave.get_centro() :", nave.get_centro())
+print("nave.get_posicao() :", nave.get_posicao())
+
+i = 0
+
+jogoAtivo = True
+
+while jogoAtivo:
+    i+=1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogoAtivo = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                jogoAtivo = False
+
+    tela.fill((0, 0, 0))
+    nave.set_angulo_rotacao(i)
+    nave_rot = nave.rotacaoCentralizada(i)
+    tela.blit(nave_rot[0], nave_rot[1])
+
+    nave.set_posicao((0,0))
+
+    tela.flip()
+    tela.fps(60)
+
+    print(nave.get_angulo_rotacao())
+
+pygame.quit()
--- NovoObjeto.py
+++ NovoObjeto.py
@@ -1,15 +1,16 @@
 import pygame
 
-class New_objeto:
+class Novo_objeto:
     def __init__(self, surface, posicao):
         self.__surface = pygame.image.load(surface)
-        self.__posicao = posicao
         self.__tamanho = self.__surface.get_rect()[2], self.__surface.get_rect()[3]
+        self.__posicao = posicao[0], posicao[1]
+        self.__centro = (self.__posicao[0] + (self.__tamanho[0]/2)), (self.__posicao[1] + (self.__tamanho[1]/2))
         self.__velocidade = 0, 0
         self.__friccao = 0
         self.__angulo_rotacao = 0
         self.__velocidade_rotacao = 0
-        #self.__rect =self.__surface.get_rect()
+        self.__rect =self.__surface.get_rect()
         self.__propulsor = False
         self.__colidiu_tela = False
         self.__rotacionou = False, False
@@ -30,13 +31,28 @@ class New_objeto:
         return self.__tamanho
 
     def set_tamanho(self, novo_tamanho):
+        self.__surface = pygame.transform.scale(self.__surface, (novo_tamanho))
+        novo_centro = novo_tamanho[0]/2, novo_tamanho[1]/2
+        self.set_centro(novo_centro)
         self.__tamanho = novo_tamanho
+
+
+    def get_centro(self):
+        return self.__centro
+
+    def set_centro(self, novo_centro):
+        self.__centro = novo_centro
+
+
 
     def get_velocidade(self):
         return self.__velocidade
 
     def set_velocidade(self, nova_velocidade):
         self.__velocidade = nova_velocidade
+
+    def get_rect(self):
+        return self.__rect
 
     def get_friccao(self):
         return self.__friccao
@@ -55,3 +71,28 @@ class New_objeto:
 
     def set_velocidade_rotacao(self, nova_velocidade_rotacao):
         self.__velocidade_rotacao = nova_velocidade_rotacao
+
+    def get_propulsor_ativo(self):
+        return self.__propulsor
+
+    def set_propulsor_ativo(self, booleana):
+        self.__propulsor = booleana
+
+    def get_colidiu_tela(self):
+        return self.__colidiu_tela
+
+    def set_colidiu_tela(self, booleana):
+        self.__colidiu_tela = booleana
+
+    def get_rotacionou(self):
+        return self.__rotacionou
+
+    def rotacaoCentralizada(self,angulo):
+
+        surface = pygame.transform.rotate(self.get_surface(), angulo)
+        nova_posicao = surface.get_rect(center=self.get_centro())
+        nova_posicao[0] += self.get_posicao()[0]
+        nova_posicao[1] += self.get_posicao()[1]
+
+        return surface ,nova_posicao
+
