e5f94ae4703613f2a1205e56ce68317240572253
Wellington Maciel <welgt.maciel@gmail.com>
2020-05-04 12:05:34 -0300

    reformulacao e adicao das classes que criam tela e controlam as configuracoes

---
+++ NovaTela.py
@@ -0,0 +1,39 @@
+import pygame
+class Nova_tela:
+    def __init__(self, titulo, resolucao):
+        self.__tela = pygame.display.set_mode(resolucao)
+        self.__titulo = pygame.display.set_caption(titulo)
+        self.__titulo_da_tela = titulo
+        self.__clock =pygame.time.Clock()
+        self.__fps = 60
+
+    def get_resolucao_tela(self):
+        return self.__tela
+
+    def set_resolucao_tela(self, nova_resolucao):
+        self.__tela = pygame.display.set_mode(nova_resolucao)
+
+    def get_titulo_tela(self):
+        return self.__titulo_da_tela
+
+    def set_titulo_tela(self, novo_titulo):
+        self.__titulo_da_tela = novo_titulo
+
+    def get_fps(self):
+        return self.__fps
+
+
+    def blit(self, surface_obj, posicao_obj):
+        self.__tela.blit(surface_obj, posicao_obj)
+
+    def fill(self, cor):
+        self.__tela.fill(cor)
+
+    def flip(self):
+        pygame.display.flip()
+
+    def set_fps(self, fps):
+        self.__clock.tick(fps)
+
+    def draw(self, cor, lista_vertice):
+        pygame.draw.polygon(self.__tela, cor, lista_vertice)
