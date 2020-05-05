cc831f55c7da9d5eb2f2917031740d6169f91173
Wellington Maciel <welgt.maciel@gmail.com>
2020-05-05 17:14:50 -0300

    aplicado um laço para desenhar o terreno dinamicamente e de acordo com o tamanho da tela

--- Mapa.py
+++ Mapa.py
@@ -14,21 +14,13 @@ class Mapa_do_jogo:
 
     def desenha_solo(self, tela):
         #print("desenhando solo")
-        # poligono_propulsor = pygame.draw.polygon(gamePlay.tela, WHITE, [(300,600), (295,618), (824,407), (800,400)],0)
-
-
-
         retangulo = pygame.Rect(500, 300, 60, 60)
         tela.draw_rect(WHITE, retangulo)
-
-
         #terreno = [(100, 100), (500, 100),(500, 500), (100, 500)]
-
 
         if self.__desenha == True:
             randon_1 = random.randint(400, 550)
             randon_2 = random.randint(400, 550)
-
             randon_3 = random.randint(400, 550)
             randon_4 = random.randint(400, 550)
             randon_5 = random.randint(400, 550)
@@ -37,13 +29,46 @@ class Mapa_do_jogo:
             randon_8 = random.randint(400, 550)
             randon_9 = random.randint(400, 550)
             randon_10 = random.randint(400, 550)
-
+            """ 
             self.__terreno = [(0, randon_1),
 
-                              (240, randon_3), (360, randon_4), (480, randon_5), (600, randon_6), (720, randon_7), (840, randon_8),(950, randon_9), (1080, randon_8),
+                              (240, randon_3), (360, randon_4), (480, randon_5), (600, randon_6), (720, randon_7), (840, randon_8),(950, randon_9), (1080, randon_8),(1200, randon_2),
 
-                              (1200, randon_2),
+
                               (1200, 1200), (0, 600)] # nao pode modificar, limite da tela!
-            self.__desenha = False
+            self.__desenha = False"""
+            #tela.draw_polygon(WHITE, self.__terreno)
 
+            self.__terreno = ((0, 0),(0, 0), (0, 0), (0, 0),(0, 0),
+                              (0, 0),(0, 0), (0, 0), (0, 0),(0, 0),
+
+                              (0, 0), (0, 0), (0, 0), (0, 0), (0, 0),
+                              (0, 0), (0, 0), (0, 0), (0, 0), (0, 0),
+
+                              (1200, 1200), (0, 600))  # nao pode modificar, limite da tela!
+
+            x = 0
+            i = 1
+            porcento_h = (RESOLUCAO[1]/1000) * 200 # 20% da tela
+            novo_terreno = []
+            for vertice in self.__terreno:
+                rand = random.randint((RESOLUCAO[1]-porcento_h), (RESOLUCAO[1]-10))
+                vertice_aux = ((vertice[0] + x), (vertice[1] + rand))
+                if i == 21:
+                    vertice_aux = ((RESOLUCAO[0]), (RESOLUCAO[1]))
+                if i == 22:
+                    vertice_aux = ((0), (RESOLUCAO[0]))
+                print("i :", i)
+                print("x :", x)
+                print(rand)
+                x += RESOLUCAO[0]/20
+                i += 1
+
+                novo_terreno.append(vertice_aux)
+
+            self.__terreno = novo_terreno
+            print(self.__terreno)
+
+            self.__desenha = False
         tela.draw_polygon(WHITE, self.__terreno)
+
